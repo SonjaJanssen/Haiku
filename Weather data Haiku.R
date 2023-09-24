2e82bcdb788fd27c574b4a571a6d27ec779f8557
Sonja Janssen-Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-09-25 01:33:46 +0200

    Add files via upload

---
+++ Weather data Haiku.R
@@ -0,0 +1,43 @@
+# A Haiku of the weather data
+# Created by Sonja Janssen-Sahebzad
+
+# Load necessary packages
+library(stringr)
+library(magrittr)
+
+# Simulated weather data
+weather_data <- data.frame(
+    date = seq(as.Date("2023-01-01"), as.Date("2023-01-07"), by = "days"),
+    temperature = c(10, 12, 11, 9, 8, 7, 6),
+    precipitation = c(0, 0, 2, 1, 0, 0, 3)
+)
+
+# Extract relevant weather information
+avg_temp <- mean(weather_data$temperature)
+total_precipitation <- sum(weather_data$precipitation)
+
+# Create a haiku
+haiku <- str_wrap(
+    paste(
+        "Amidst winter's chill,", 
+        "Subdued warmth, soft rains embrace,", 
+        "Nature's quiet grace."
+    ),
+    width = 30
+)
+
+# Display the haiku with weather data
+cat("Average Temperature:", avg_temp, "°C\n")      # # Average Temperature: 9 °C
+cat("Total Precipitation:", total_precipitation, "mm\n\n")  # # Total Precipitation: 6 mm
+cat(haiku)
+
+# outcome:
+# > cat(haiku)
+# Amidst winter's chill, Subdued
+# warmth, soft rains embrace,
+# Nature's quiet grace.
+
+
+
+
+
